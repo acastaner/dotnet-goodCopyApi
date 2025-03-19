ce42ee9fb60fa291d8742ba134a35d0c45c40760
Arnaud <arnaud.castaner@outlook.com>
2025-03-19 22:51:15 +0100

    Removed Data Annotations in Product model

--- Models/Product.cs
+++ Models/Product.cs
@@ -1,18 +1,14 @@
 namespace GoodCopyApi.Models;
-using System.ComponentModel.DataAnnotations;
 using System;
 
 public class Product
 {
     public int Id { get; set; }
 
-    [Required]
     public string Name { get; set; }
 
-    [Required]
     public double Price { get; set; }
 
-    [Required]
     public int Stock { get; set; }
 
     public Product()
